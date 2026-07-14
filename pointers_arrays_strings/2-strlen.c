d649b465e7d8ae8b6c4ea9724cb37ff67b2998fa
yaser8541 <443017396@sm.imamu.edu.sa>
2026-07-14 18:14:40 +0300

    2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,17 @@
 /**
- * swap_int - swaps the values of two integers
- * @a: pointer to the first integer
- * @b: pointer to the second integer
+ * _strlen - returns the length of a string
+ * @s: string to check
  *
- * Return: Nothing.
+ * Return: length of the string.
  */
 int _strlen(char *s)
 {
 	int len = 0;
 
 	while (*s != '\0')
-    {
-        len++;
-        s++;
-    }
-	return len;
+	{
+		len++;
+		s++;
+	}
+	return (len);
 }
