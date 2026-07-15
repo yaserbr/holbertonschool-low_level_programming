d690841b246d7d5aea853a0ae86d2b062811b32c
yaser8541 <443017396@sm.imamu.edu.sa>
2026-07-15 20:10:57 +0300

    5

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -1,6 +1,43 @@
 #include "main.h"
 
 /**
+ * num_len - returns the length of a number string
+ * @num: number string
+ *
+ * Return: length of num.
+ */
+int num_len(char *num)
+{
+	int i;
+
+	i = 0;
+	while (num[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
+ * move_result - moves the result to the beginning of the buffer
+ * @r: buffer used to store the result
+ * @start: first digit of the result
+ * @end: end of the result area
+ *
+ * Return: pointer to r.
+ */
+char *move_result(char *r, int start, int end)
+{
+	int i;
+
+	i = 0;
+	while (start < end)
+		r[i++] = r[start++];
+	r[i] = '\0';
+
+	return (r);
+}
+
+/**
  * infinite_add - adds two numbers
  * @n1: first number
  * @n2: second number
@@ -16,21 +53,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int k;
 	int sum;
 	int carry;
-	int start;
 
 	if (size_r <= 1)
 		return (0);
 
-	i = 0;
-	while (n1[i] != '\0')
-		i++;
-
-	j = 0;
-	while (n2[j] != '\0')
-		j++;
-
-	i--;
-	j--;
+	i = num_len(n1) - 1;
+	j = num_len(n2) - 1;
 	k = size_r - 2;
 	carry = 0;
 	while (i >= 0 || j >= 0 || carry)
@@ -49,11 +77,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		k--;
 	}
 
-	start = k + 1;
-	i = 0;
-	while (start < size_r - 1)
-		r[i++] = r[start++];
-	r[i] = '\0';
-
-	return (r);
+	return (move_result(r, k + 1, size_r - 1));
 }
