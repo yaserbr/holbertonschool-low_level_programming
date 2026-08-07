4c43dd7b376b02828e5ed931c0e2b6c6ee171643
yaser8541 <443017396@sm.imamu.edu.sa>
2026-08-07 22:29:25 +0300

    adv

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * before_main - prints a message before main is executed
+ */
+void before_main(void) __attribute__((constructor));
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
