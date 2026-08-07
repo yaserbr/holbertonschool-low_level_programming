7b747802cf0250313bddede9e02a613fbc9d6b3c
yaser8541 <443017396@sm.imamu.edu.sa>
2026-08-07 22:12:19 +0300

    5

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include "lists.h"
-
+#include <stdlib.h>
+#include <string.h>
 
 list_t *add_node(list_t **head, const char *str){
     
     list_t *new_node = malloc(sizeof(list_t));
     if (new_node == NULL) {
-        return NULL; // Memory allocation failed
+        return NULL; 
     }
 
-    new_node->str = strdup(str); // Duplicate the string
+    new_node->str = strdup(str);
     if (new_node->str == NULL) {
-        free(new_node); // Free the allocated memory for the node
-        return NULL; // Memory allocation failed
+        free(new_node); 
+        return NULL; 
     }
 
-    new_node->len = strlen(str); // Set the length of the string
-    new_node->next = *head; // Point to the current head of the list
-    *head = new_node; // Update the head to point to the new node
+    new_node->len = strlen(str); 
+    new_node->next = *head;
+    *head = new_node; 
 
-    return new_node; // Return the newly created node
+    return new_node; 
 }
