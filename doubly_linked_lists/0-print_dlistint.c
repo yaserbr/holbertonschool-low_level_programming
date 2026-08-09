2f677349070b363551c05e5cc2dfe004d68de5e6
yaser8541 <443017396@sm.imamu.edu.sa>
2026-08-09 19:05:35 +0300

    1

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -9,19 +9,19 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count;
+	size_t count;
 
-    count = 0;
+	count = 0;
 
-    if (h == NULL)
-        return (0);
-        
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
+	if (h == NULL)
+		return (0);
 
-    return (count);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
 }
