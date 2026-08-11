517533490648c78f93cbcc88721e01da08dafc9f
yaser8541 <443017396@sm.imamu.edu.sa>
2026-08-11 19:47:39 +0300

    newday

---
+++ hash_tables/hash_tables.h
@@ -0,0 +1,32 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+/**
+ * struct hash_node_s - Node of a hash table.
+ * @key: The key, string.
+ * @value: The value corresponding to a key.
+ * @next: A pointer to the next node of the list.
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - Hash table data structure.
+ * @size: The size of the array.
+ * @array: An array of size @size.
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+
+#endif /* HASH_TABLES_H */
