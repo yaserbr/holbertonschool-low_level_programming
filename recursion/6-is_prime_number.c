474c5d78db6e2db3a7d39268ad7d205cfbbc4360
yaser8541 <443017396@sm.imamu.edu.sa>
2026-08-18 19:35:54 +0300

    4

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,11 +9,11 @@
  */
 int prime_helper(int n, int i)
 {
-    if (n % i == 0)
-        return (0);
-    if (i * i > n)
-        return (1);
-    return (prime_helper(n, i + 1));
+	if (n % i == 0)
+		return (0);
+	if (i * i > n)
+		return (1);
+	return (prime_helper(n, i + 1));
 }
 
 /**
@@ -24,8 +24,8 @@ int prime_helper(int n, int i)
  */
 int is_prime_number(int n)
 {
-    if (n <= 1)
-        return (0);
+	if (n <= 1)
+		return (0);
 
-    return (prime_helper(n, 2));
+	return (prime_helper(n, 2));
 }
