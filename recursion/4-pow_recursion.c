9baff36cdc9f1315d5e02990ebdde337c58ba257
yaser8541 <443017396@sm.imamu.edu.sa>
2026-08-18 18:37:14 +0300

    5

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a given number
+ * @n: the number to calculate the factorial of
+ *
+ * Return: the factorial of n, or -1 if n is negative
+ */
+
+int _pow_recursion(int x, int y)
+{
+    if (y < 0)
+        return (-1);
+    if (y == 0)
+        return (1);
+    return (x * _pow_recursion(x, y - 1));
+}
